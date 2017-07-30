c2ea78a37ff2ca1cb897cc610f39db72a553d2d4
Laura Vallhonrat <vallhonratlaura@gmail.com>
2017-07-30 17:26:09 +0200

    component paths fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ButtonsComponent } from './buttons/buttons.component';
-import { ListIncComponent } from './list-inc/list-inc.component';
-import { ListExpComponent } from './list-exp/list-exp.component';
-import { AddIncComponent } from './add-inc/add-inc.component';
-import { AddExpComponent } from './add-exp/add-exp.component';
+import { ButtonsComponent } from './add-features/buttons/buttons.component';
+import { ListIncComponent } from './list-features/list-inc/list-inc.component';
+import { ListExpComponent } from './list-features/list-exp/list-exp.component';
+import { AddIncComponent } from './add-features/add-inc/add-inc.component';
+import { AddExpComponent } from './add-features/add-exp/add-exp.component';
 
 @NgModule({
   declarations: [
