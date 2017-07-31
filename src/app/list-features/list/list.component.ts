2e540f602c75beffabb618ac30be03f3ee573991
Emilie G <adamanska@gmail.com>
2017-07-31 12:35:41 +0200

    list comp

--- src/app/list-features/list/list.component.ts
+++ src/app/list-features/list/list.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  exp = [];
+  inc = [];
 
   constructor() { }
 
