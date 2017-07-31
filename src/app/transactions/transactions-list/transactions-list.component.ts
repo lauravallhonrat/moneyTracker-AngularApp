cd7eb738c70412409602a9382ec3680951df5474
Emilie G <adamanska@gmail.com>
2017-07-31 16:49:33 +0200

    trans item appearing

--- src/app/transactions/transactions-list/transactions-list.component.ts
+++ src/app/transactions/transactions-list/transactions-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import {Transaction} from './transactions.model';
+// we imported the model that we will call.
 
 @Component({
   selector: 'app-transactions-list',
@@ -10,8 +11,13 @@ import {Transaction} from './transactions.model';
 export class TransactionsListComponent implements OnInit {
 
   transactions: Transaction[] = [
-    new Transaction ('some category', 34, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTtbljyNDGAqvVwzfKo5tk4jtnZubU4SHrKtB7wl7S0qALzD2AUg', new Date("MM/dd/yyyy"))
+    new Transaction ('some category', 34, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTtbljyNDGAqvVwzfKo5tk4jtnZubU4SHrKtB7wl7S0qALzD2AUg', new Date("11/11/2093")),
+    new Transaction ('some other category', 5334, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTtbljyNDGAqvVwzfKo5tk4jtnZubU4SHrKtB7wl7S0qALzD2AUg', new Date("03/06/2093"))
   ];
+  // transactions is a given name, Transaction[] refers to the Transaction class name from the transaction.model.ts
+  // we create a new Transaction with the different elements we defined in the model.
+  // we will use them in the HTML
+  
   
 
   constructor() { }
