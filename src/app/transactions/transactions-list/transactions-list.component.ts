ebda51aee8b83c66c3c39658e0d265f60e22ec1e
Emilie G <adamanska@gmail.com>
2017-08-01 19:21:43 +0200

    services under construction

--- src/app/transactions/transactions-list/transactions-list.component.ts
+++ src/app/transactions/transactions-list/transactions-list.component.ts
@@ -12,10 +12,7 @@ import {Transaction} from '../transactions.model';
 export class TransactionsListComponent implements OnInit {
   //create new event emitter passing transaction as a type
   @Output() transactionWasSelected = new EventEmitter<Transaction>();
-  transactions: Transaction[] = [
-    new Transaction ('some category', 34, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTtbljyNDGAqvVwzfKo5tk4jtnZubU4SHrKtB7wl7S0qALzD2AUg', new Date("11/11/2093")),
-    new Transaction ('some other category', 5334, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTtbljyNDGAqvVwzfKo5tk4jtnZubU4SHrKtB7wl7S0qALzD2AUg', new Date("03/06/2093"))
-  ];
+  
   // transactions is a given name, Transaction[] refers to the Transaction class name from the transaction.model.ts
   // we create a new Transaction with the different elements we defined in the model.
   // we will use them in the HTML
--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Transaction } from "./transactions.model";
+import {TransactionService} from "./transactions.services"
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
-  styleUrls: ['./transactions.component.css']
+  styleUrls: ['./transactions.component.css'],
+  providers : [TransactionService]
 })
 export class TransactionsComponent implements OnInit {
 
