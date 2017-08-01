3db4b8f1e43d98a6e6e1fd55da961b2fba79341b
Laura Vallhonrat <vallhonratlaura@gmail.com>
2017-08-01 12:09:12 +0200

    transaction items events

--- src/app/transactions/transactions-list/transactions-items/transactions-items.component.ts
+++ src/app/transactions/transactions-list/transactions-items/transactions-items.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+//import input decorator
+import {Transaction} from '../transactions.model';
+//import transaction type from transactions model file
 
 @Component({
   selector: 'app-transactions-items',
@@ -6,8 +9,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transactions-items.component.css']
 })
 export class TransactionsItemsComponent implements OnInit {
-  
-
+  //transaction of type transaction (coming from the transaction model file)
+  //input decorator gets the data from outside
+  //this will be defined on the transaction list html file
+  @Input() transaction: Transaction;
   constructor() { }
 
   ngOnInit() {
