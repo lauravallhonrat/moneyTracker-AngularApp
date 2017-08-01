c68e2a42ace9f2aa0ab2b3c6b2fb54449142fc18
Laura Vallhonrat <vallhonratlaura@gmail.com>
2017-08-01 11:41:10 +0200

    expenses overlay navigation event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   loadedOverlay = 'expense';
-
+//loads overlay expenses html
   onNavigate(overlay: string){
     this.loadedOverlay = overlay;
   }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, EventEmitter,Output } from '@angular/core';
-//import eventEmitter and output from core
+//import eventEmitter and output decorator from core
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  //call new even emitter for click event
+  //call new even emitter for click event and adds output decorator, the click event contains expense keyword (which will be called on the app component later on)
   @Output() featureSelected = new EventEmitter<string>();
   constructor() { }
 
