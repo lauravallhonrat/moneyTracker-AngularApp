369cb2b10e73ce824238c6220cc02c8acd6a0248
Laura Vallhonrat <vallhonratlaura@gmail.com>
2017-08-01 13:33:23 +0200

    Fixed a pesky bug/error

--- src/app/transactions/transactions-list/transactions-items/transactions-items.component.ts
+++ src/app/transactions/transactions-list/transactions-items/transactions-items.component.ts
@@ -12,6 +12,11 @@ export class TransactionsItemsComponent implements OnInit {
   //transaction of type transaction (coming from the transaction model file)
   //input decorator gets the data from outside
   //this will be defined on the transaction list html file
+@Input() item: Transaction;
+// This item here is used in trans-list-comp-html in the ngFor
+// I ADDED THAT !!!!!!
+
+
   @Input() transaction: Transaction;
   constructor() { }
 
