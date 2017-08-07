e52743da93bbdd2049f4d2e40f13f0faf0102640
Emilie G <adamanska@gmail.com>
2017-08-07 12:15:15 +0200

    Trying to make the delete for the transactions - no success so far

--- src/app/transactions/transactions-list/transactions-items/transactions-items.component.ts
+++ src/app/transactions/transactions-list/transactions-items/transactions-items.component.ts
@@ -3,12 +3,30 @@ import {Transaction} from '../../transactions.model';
 import {TransactionService} from '../../transactions.service';
 
 
+
 @Component({
   selector: 'app-transactions-items',
   templateUrl: './transactions-items.component.html',
   styleUrls: ['./transactions-items.component.css']
 })
+
 export class TransactionsItemsComponent implements OnInit {
+
+  // deleteThing: boolean = false;
+  // showThing:boolean = true;
+  // deleteThat = true;
+  
+  // myFunction(){
+  //   console.log("click bande");
+  //   this.deleteIsHidden = false;
+  //   this.showThing = false;
+  // }
+  // nope(){
+  //   console.log("nope");
+  //   this.deleteThing = false;
+  //   this.showThing = true;
+  //   console.log(this.deleteThing, this.showThing);
+  // }
 
   @Input() transaction: Transaction;
   //transaction coming from transaction model
@@ -16,8 +34,11 @@ export class TransactionsItemsComponent implements OnInit {
 
   ngOnInit() {
   }
+
   onSelected(){
     //this.transactionSelected.emit();//call emit when emitEmitter is used
     //this.transactionService.getThoseTransactions.emit(this.transaction);
   }
+
+  
 }
